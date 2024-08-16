8942cb8573053637c68e7e4dbbc30e9d09c51c74
Denis Bunak <xisi926@ukr.net>
2024-08-16 11:52:17 +0300

    Refactored method play in class TicTacToe

--- src/main/java/global/goit/edu/tictactoe/TicTacToe.java
+++ src/main/java/global/goit/edu/tictactoe/TicTacToe.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private static byte input;
-    private static byte rand;
-    private static byte i;
-    private static boolean boxEmpty = false;
-    private static boolean boxAvailable = false;
-    private static byte winner = 0;
-    private static char box[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    private static String parting = "Good Bye!";
-    private static Scanner scan = new Scanner(System.in);
+    private int i;
+    private boolean boxEmpty = false;
+    private boolean boxAvailable = false;
+    private byte winner = 0;
+    private char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    private static final String PARTING = "Good Bye!";
+    private static final char PLAYER_MOVE = 'X';
+    private static final char COMPUTER_MOVE = 'O';
+    private static final Scanner scan = new Scanner(System.in);
+    private static final TicTacToe game = new TicTacToe();
 
-    public static void play() {
+    public void play() {
 
         System.out.println("Enter box number to select. Enjoy!\n");
 
@@ -23,93 +24,114 @@ public class TicTacToe {
 
             emptyTheBox();
 
-            if (winner == 1) {
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            if (checkingWinner()) break;
 
-                if (isContinueGame()) {
-                    TicTacToe.play();
-                } else {
-                    System.out.println(parting);
-                    break;
-                }
-            } else if (winner == 2) {
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            checkingPlayerMove();
 
-                if (isContinueGame()) {
-                    TicTacToe.play();
-                } else {
-                    System.out.println(parting);
-                    break;
-                }
-            } else if (winner == 3) {
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            if (checkingForWinner(PLAYER_MOVE)
+            || checkingIfDraw()) continue;
 
-                if (isContinueGame()) {
-                    TicTacToe.play();
-                } else {
-                    System.out.println(parting);
-                    break;
-                }
-            }
+            checkingComputerMove();
 
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                } else
-                    System.out.println("Invalid input. Enter again.");
-            }
+            checkingForWinner(COMPUTER_MOVE);
+        }
+    }
 
-            if ((box[0] == 'X' && box[1] == 'X' && box[2] == 'X') || (box[3] == 'X' && box[4] == 'X' && box[5] == 'X')
-                    || (box[6] == 'X' && box[7] == 'X' && box[8] == 'X')
-                    || (box[0] == 'X' && box[3] == 'X' && box[6] == 'X')
-                    || (box[1] == 'X' && box[4] == 'X' && box[7] == 'X')
-                    || (box[2] == 'X' && box[5] == 'X' && box[8] == 'X')
-                    || (box[0] == 'X' && box[4] == 'X' && box[8] == 'X')
-                    || (box[2] == 'X' && box[4] == 'X' && box[6] == 'X')) {
+    private boolean checkingForWinner(char move) {
+        if ((box[0] == move && box[1] == move && box[2] == move) || (box[3] == move && box[4] == move && box[5] == move)
+                || (box[6] == move && box[7] == move && box[8] == move)
+                || (box[0] == move && box[3] == move && box[6] == move)
+                || (box[1] == move && box[4] == move && box[7] == move)
+                || (box[2] == move && box[5] == move && box[8] == move)
+                || (box[0] == move && box[4] == move && box[8] == move)
+                || (box[2] == move && box[4] == move && box[6] == move)) {
+
+            if (move == PLAYER_MOVE) {
                 winner = 1;
-                continue;
-            }
-
-            boxAvailable = false;
-            for (i = 0; i < 9; i++) {
-                if (box[i] != 'X' && box[i] != 'O') {
-                    boxAvailable = true;
-                    break;
-                }
-            }
-
-            if (boxAvailable == false) {
-                winner = 3;
-                continue;
-            }
-
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
-
-            if ((box[0] == 'O' && box[1] == 'O' && box[2] == 'O') || (box[3] == 'O' && box[4] == 'O' && box[5] == 'O')
-                    || (box[6] == 'O' && box[7] == 'O' && box[8] == 'O')
-                    || (box[0] == 'O' && box[3] == 'O' && box[6] == 'O')
-                    || (box[1] == 'O' && box[4] == 'O' && box[7] == 'O')
-                    || (box[2] == 'O' && box[5] == 'O' && box[8] == 'O')
-                    || (box[0] == 'O' && box[4] == 'O' && box[8] == 'O')
-                    || (box[2] == 'O' && box[4] == 'O' && box[6] == 'O')) {
+                return true;
+            } else if (move == COMPUTER_MOVE) {
                 winner = 2;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkingComputerMove() {
+        while (true) {
+            byte rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
+            if (box[rand - 1] != PLAYER_MOVE && box[rand - 1] != COMPUTER_MOVE) {
+                box[rand - 1] = COMPUTER_MOVE;
+                break;
             }
         }
     }
 
-    private static void emptyTheBox() {
+    private boolean checkingIfDraw() {
+        boxAvailable = false;
+        for (i = 0; i < 9; i++) {
+            if (box[i] != PLAYER_MOVE && box[i] != COMPUTER_MOVE) {
+                boxAvailable = true;
+                break;
+            }
+        }
+
+        if (boxAvailable == false) {
+            winner = 3;
+            return true;
+        }
+        return false;
+    }
+
+    private void checkingPlayerMove() {
+        while (true) {
+            byte input = scan.nextByte();
+            if (input > 0 && input < 10) {
+                if (box[input - 1] == PLAYER_MOVE || box[input - 1] == COMPUTER_MOVE)
+                    System.out.println("That one is already in use. Enter another.");
+                else {
+                    box[input - 1] = PLAYER_MOVE;
+                    break;
+                }
+            } else {
+                System.out.println("Invalid input. Enter again.");
+            }
+        }
+    }
+
+    private boolean checkingWinner() {
+        if (winner == 1) {
+            System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+
+            if (isContinueGame()) {
+                game.play();
+            } else {
+                System.out.println(PARTING);
+                return true;
+            }
+        } else if (winner == 2) {
+            System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+
+            if (isContinueGame()) {
+                game.play();
+            } else {
+                System.out.println(PARTING);
+                return true;
+            }
+        } else if (winner == 3) {
+            System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+
+            if (isContinueGame()) {
+                game.play();
+            } else {
+                System.out.println(PARTING);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void emptyTheBox() {
         if (!boxEmpty) {
             for (i = 0; i < 9; i++)
                 box[i] = ' ';
@@ -117,7 +139,7 @@ public class TicTacToe {
         }
     }
 
-    private static void presentationGame() {
+    private void presentationGame() {
         System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
         System.out.println("-----------");
         System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
@@ -125,7 +147,7 @@ public class TicTacToe {
         System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
     }
 
-    private static boolean isContinueGame() {
+    private boolean isContinueGame() {
         String inputAnswer;
         String questionToPlayer = "Enter Yes(Y) or No (N)";
 
@@ -160,6 +182,7 @@ public class TicTacToe {
                     restartGame();
                     return true;
                 } else if (inputAnswer.equals("n")) {
+                    System.exit(0);
                     return false;
                 } else {
                     System.out.println("\n");
@@ -170,7 +193,7 @@ public class TicTacToe {
     }
 
     //Обнуление всех значений для новой игры
-    private static void restartGame() {
+    private void restartGame() {
         boxEmpty = false;
         boxAvailable = false;
         winner = 0;
